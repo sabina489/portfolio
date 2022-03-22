689b84743275da5c9a978a928657013e002cd8c9
sabina489 <Karkisabina869@gmail.com>
2022-03-22 16:22:14 +0545

    create serialzers class

---
+++ sabina/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from sabina.models import Home, About, Profile, Category, Skills, Project, Education
+
+class SabinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SabinaSerializer
+        fields = 
